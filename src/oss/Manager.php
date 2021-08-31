a8e8d29cf877a9d3641aefa20f44ef093b8978a9
china-li-shuo <676050059@qq.com>
2021-08-31 16:03:57 +0800

    第一个composer包

---
+++ src/oss/Manager.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Author: 李硕
+ * Email: kezuo@foxmail.com
+ * Date: 2021/8/31
+ * Time: 15:59
+ */
+
+namespace lishuo\src\oss;
+
+class Manager
+{
+
+    public function index()
+    {
+        return 'hello world';
+    }
+}
