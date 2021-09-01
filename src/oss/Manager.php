02338f409bfdf876cc7c5b482883817d28a5be35
china-li-shuo <676050059@qq.com>
2021-09-01 10:08:58 +0800

    update Manager

--- src/oss/Manager.php
+++ src/oss/Manager.php
@@ -15,4 +15,29 @@ class Manager
     {
         return 'hello world';
     }
+
+    /**
+     * 获取指定云存储实例
+     * @param string $type 云存储类型，阿里云：aliyun、腾讯云：tencent、七牛云：qiniu
+     * @return Aliyun|Tencent 具体类型云存储实例
+     * @throws NonsupportStorageTypeException
+     */
+    public static function storage(string $type)
+    {
+        $storage = null;
+        switch ($type) {
+            case "aliyun":
+                $storage = new Aliyun();
+                break;
+            case "tencent":
+                $storage = new Tencent();
+                break;
+            case "qiniu":
+                $storage = new Qiniu();
+                break;
+            default:
+                throw new NonsupportStorageTypeException();
+        }
+        return $storage;
+    }
 }
